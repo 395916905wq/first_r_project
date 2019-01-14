c8623ccac508ddaf4aa2f535aa8906ed64084a44
395916905wq <knowbox_wq@knowbox-wqdeMacBook-Pro.local>
2019-01-14 17:56:55 +0800

    prox

--- db_con.R
+++ db_con.R
@@ -1,16 +1,17 @@
 library(dbplyr)
 library(tidyverse)
 
-
+#wuqiang@106.75.22.248 -L 5000:10.10.70.156:22 -NnT &
 con <- DBI::dbConnect(RPostgreSQL::PostgreSQL(), 
                       host = '127.0.0.1',
                       user = "wuqiang",
-                      port = 5432,
+                      port = 5000,
                       password = "wuqiang572",
                       dbname="octopus_susuan")
 
 
-o_teacher<-tbl(con, "o_teacher")
+o_teacher<-tbl(con, "o_teacher") %>% 
+  filter(student_count>=20)
 
 dbDisconnect(con)
 
